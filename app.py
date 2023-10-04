0f6bc770130bafd5ce8c5dbd06768f2246c7a8c5
arasaka206 <146959954+arasaka206@users.noreply.github.com>
2023-10-04 21:02:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-import tensorflow as tf
-import numpy as np
-import pandas as pd
+# import tensorflow as tf
+# import numpy as np
+# import pandas as pd
+
+st.title('Ứng dụng dự đoán dư lượng thuốc trừ sâu')
