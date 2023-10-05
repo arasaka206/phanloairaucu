e00c942b4fbb43b1307a33e159984d54bd322dd9
strikerevoler <146959954+arasaka206@users.noreply.github.com>
2023-10-05 22:59:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
 import streamlit as st
 
-# import tensorflow as tf
-# import numpy as np
-# import pandas as pd
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import keras
+import cv2
 
 st.title('Ứng dụng dự đoán dư lượng thuốc trừ sâu')
