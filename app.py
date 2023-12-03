1c47a40e6c98975f49f7c71c34aeebf27145400f
Trần Mạnh Dũng <146959954+arasaka206@users.noreply.github.com>
2023-12-03 21:57:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,15 +56,16 @@ else:
         class_names = ['chuaphun', 'phun5ngay']
 
         result = class_names[np.argmax(pred)]
+	slot.text(pred)
 	
         
         if result == "phun5ngay":
             statement = str('Kết quả chẩn đoán: **Rau đã phun thuốc trừ sâu trong vòng dưới 5 ngày**')
             st.error(statement)
-	    slot.text(pred)
+	    
         if result == "chuaphun":
             statement = str('Kết quả chẩn đoán: **Rau chưa được phun thuốc trừ sâu**')
             st.success(statement)
-	    slot.text(pred)
+	    
 	
 	slot.success('Hoàn thành chẩn đoán!')
