4cc2243a06e3faad4b4016bf338cbf4d819921be
Trần Mạnh Dũng <146959954+arasaka206@users.noreply.github.com>
2023-12-03 21:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ else:
 
         result = class_names[np.argmax(pred)]
         
-        if reuslt == 0:
+        if reuslt == "phun5ngay":
             statement = str('Kết quả chẩn đoán: **Rau đã phun thuốc trừ sâu trong vòng dưới 5 ngày**')
             st.error(statement)
         else:
